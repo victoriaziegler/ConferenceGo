bee4f2e109998e4cf35abfe48e5e58dbd63dc844
Victoria Ziegler <toriziegler@cox.net>
2022-07-12 16:14:29 -0700

    Sixth commit - Attendees microservice

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee
-from events.models import Conference
+from .models import Attendee, ConferenceVO
 from django.views.decorators.http import require_http_methods
 import json
 
@@ -11,23 +10,29 @@ class AttendeeListEncoder(ModelEncoder):
     properties = ["name"]
 
 
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
+
 class AttendeeDetailEncoder(ModelEncoder):
     model = Attendee
     properties = [
         "email",
         "name",
         "company_name",
+        "created",
         "conference",
     ]
-
-    def get_extra_data(self, o):
-        return {"conference": o.conference.name}
+    encoders = {
+        "conference": ConferenceVODetailEncoder(),
+    }
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -37,9 +42,10 @@ def api_list_attendees(request, conference_id):
 
         # Get the Conference object and put it in the content dict
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = content["conference"]
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
